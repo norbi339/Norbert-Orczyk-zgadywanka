e8707e35f5ae52dcbb132e45151ae323e5ff2709
norbi339 <norbert.orczyk@onet.eu>
2019-03-17 18:53:38 +0100

    Update Program.cs

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -10,6 +10,12 @@ namespace GraMonolitycznie
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Witaj!");
+
+            Console.Write("Podaj swoje imię: ");
+            string imie = Console.ReadLine();
+
+            Console.WriteLine($"Jak się masz, {imie}!");
         }
     }
 }
