e954b3c1a204d39117c0804fe5c2d806b399e689
Orczyk Norbert <norbert.orczyk@microsoft.wsei.edu.pl>
2019-05-19 19:31:52 +0200

    Model gry

---
+++ GraZaDuzoZaMalo/ModelGry/Gra.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ModelGry
+{
+    public class Gra
+    {
+        // inner types
+        public enum Odpowiedz { ZaMalo = -1, ZaDuzo = 0, Trafiono = 1 }
+        public enum StanGry { Trwa, Odgadnieta, Poddana }
+
+        // fields
+        public StanGry Stan { get; private set; }
+
+        public readonly int ZakresOd, ZakresDo;
+
+        //--------------
+        private readonly int wylosowana;
+        public int? Wylosowana
+        {
+            get
+            {
+                if (Stan == StanGry.Poddana || Stan == StanGry.Odgadnieta)
+                    return wylosowana;
+                else
+                    return null;
+            }
+            //set { }
+        }
+        //--------------
+
+        // hiatoria gry ToDo
+        public int LicznikRuchow { get; private set; } = 0;
+
+        // constructors
+        public Gra(int a, int b)
+        {
+            ZakresOd = Math.Min(a,b);
+            ZakresDo = Math.Max(a,b);
+            wylosowana = Losuj(ZakresOd, ZakresDo);
+            Stan = StanGry.Trwa;
+        }
+
+        public Odpowiedz Ocena(int propozycja )
+        {
+            LicznikRuchow++;
+            if (propozycja < wylosowana)
+                return Odpowiedz.ZaMalo;
+            else if (propozycja > wylosowana)
+                return Odpowiedz.ZaDuzo;
+            else
+            {
+                Stan = StanGry.Odgadnieta;
+                return Odpowiedz.Trafiono;
+            }
+        }
+
+        public void Poddaj()
+        {
+            Stan = StanGry.Poddana;
+        }
+
+        /// <summary>
+        /// Generuje liczbę pseudolosową z podanego zakresu, włącznie z krańcami
+        /// </summary>
+        /// <param name="min">dowolna liczba całkowita</param>
+        /// <param name="max">dowolna liczba całkowita</param>
+        /// <returns>liczba całkowita z podanego zakresu</returns>
+        public static int Losuj(int min = 1, int max = 100)
+        {
+            if (min > max)
+            { //swap
+                int temp = min;
+                min = max;
+                max = temp;
+            }
+            Random generator = new Random();
+            return generator.Next(min, max + 1);
+        }
+    }
+}
---
+++ GraZaDuzoZaMalo/UnitTestProjectModelGry/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectModelGry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectModelGry")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12ca40c3-6e3f-4b0c-a000-91ef3b1a6c27")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
